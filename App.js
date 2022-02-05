c79f2b2fdbe736b8bf080121488024958c095edc
qbentil <Bentilshadrack72@gmail.com>
2022-02-05 16:49:41 +0000

    Class Component

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StatusBar } from 'expo-status-bar';
+
+export default class App extends React.Component {
+  render()
+  {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
